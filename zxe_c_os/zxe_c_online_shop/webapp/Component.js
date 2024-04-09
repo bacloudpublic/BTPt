70807fba7c80dc749aaa5a9e704ac37b9fafda10
BA <161347152+bacloudpublic@users.noreply.github.com>
2024-04-09 14:57:17 +0200

    Fiori elements for Public cloud

---
+++ zxe_c_os/zxe_c_online_shop/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ns.zxeconlineshop.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
